4cb27bb29e1944095b726f76d14f789b2551a690
Vlad Chernukha <forreadtr@gmail.com>
2024-07-11 20:01:47 +0300

    Complete

--- LessonTask/LessonTask.cpp
+++ LessonTask/LessonTask.cpp
@@ -2,8 +2,44 @@
 //
 
 #include <iostream>
+#include <string>
+#include <map>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string text;
+	map < string, int> count;
+
+	while (text != "Stop" && text != "stop")
+	{
+		cout << "Input your text: " << endl;
+		cin >> text;
+		if (text == "stop" || text == "Stop")
+		{
+			break;
+		}
+		if (count.find(text) == count.end())
+		{
+			/*count.insert(pair<string, int>(text, 1));*/
+			count[text] = 1;
+		}
+		else
+		{
+			count[text]++;
+		}
+	}
+
+	cout << "------------------------------------------------------------------------------------" << endl;
+
+	map<string, int>::iterator item;
+
+	for (item = count.begin(); item != count.end(); item++)
+	{
+		cout << item->first << " " << item->second << endl;
+	}
+	{
+
+	}
+
 }
